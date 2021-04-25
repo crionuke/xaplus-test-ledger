d666d0f985478d0ec91c12afa7009ffedcfe96f8
Kirill Byvshev <k@byv.sh>
2021-04-25 22:42:39 +0300

    simple generator of requests

---
+++ localEnv/generator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DELAY=10
+PORTS=(10001 10002 10003)
+SIZE=${#PORTS[@]}
+
+RQ_UID=1
+
+while :; do
+    INDEX=$(($RANDOM%SIZE))
+    PORT=${PORTS[INDEX]}
+
+    FROM_USER=$(($RANDOM%3000))
+    TO_USER=$(($RANDOM%3000))
+    COUNT=$((1+$RANDOM%1000))
+
+    if [[ ${FROM_USER} -ne ${TO_USER} ]]; then
+        CMD="curl -X POST http://localhost:${PORT}/transfer?rqUid=${RQ_UID}&fromUser=${FROM_USER}&toUser=${TO_USER}&count=${COUNT}"
+        echo
+        echo ${CMD}
+        ${CMD}
+        sleep ${DELAY}
+
+        RQ_UID=$((RQ_UID+1))
+    fi
+done
